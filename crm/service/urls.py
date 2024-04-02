7a9471e94172f00222f4127277f739cb5599e4ed
VERTEksaR <14.vert.41@gmail.com>
2024-04-02 15:01:30 +0300

    Уход от rest_framework

--- crm/service/urls.py
+++ crm/service/urls.py
@@ -2,19 +2,21 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 from service.views import (
-    ServicesAPIView,
-    ServiceAPIView,
-    CreateServiceAPIView,
-    create_service,
+    ServiceListView,
+    CreateServiceView,
+    DeleteServiceView,
+    DetailsServiceView,
+    UpdateServiceView,
 )
 
 
 app_name = 'service'
 
 urlpatterns = [
-    path('', ServicesAPIView.as_view(), name='services'),
-    # path('create/', CreateServiceAPIView.as_view(), name='service_create'),
-    path('<int:pk>/', ServiceAPIView.as_view(), name='service_detail'),
-
-    path('create/', create_service)
+    path('', TemplateView.as_view(template_name='service/_base.html')),
+    path('products/', ServiceListView.as_view(), name='products-list'),
+    path('products/<int:pk>/', DetailsServiceView.as_view(), name='products-detail'),
+    path('products/new/', CreateServiceView.as_view()),
+    path('products/<int:pk>/edit/', UpdateServiceView.as_view()),
+    path('products/<int:pk>/delete/', DeleteServiceView.as_view()),
 ]
--- crm/service/views.py
+++ crm/service/views.py
@@ -1,85 +1,46 @@
-import json
-
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework.renderers import TemplateHTMLRenderer
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DeleteView, DetailView, UpdateView
 
 from service.models import Service
 from .serializers import ServiceSerializer
 
 
-class ServicesAPIView(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-
-    @staticmethod
-    def get(request: Request) -> Response:
-        services = list(Service.objects.all())
-
-        if len(services) != 0:
-            serializer = ServiceSerializer(instance=services, many=True)
-            return Response({"services": serializer.data}, status=status.HTTP_200_OK, template_name='service/services.html')
-
-        return Response(data='Список с услугами пуст', status=status.HTTP_200_OK, template_name='service/services.html')
+class ServiceListView(ListView):
+    model = Service
+    queryset = Service.objects.all()
+    serializer_class = ServiceSerializer
+    template_name = 'service/products-list.html'
+    context_object_name = 'products'
 
 
-@api_view(http_method_names=['get', 'post'])
-def create_service(request: Request):
-    if request.method == 'POST':
-        print(request)
-        info = json.loads(request.body)
-        serializer = ServiceSerializer(data=info)
+class CreateServiceView(CreateView):
+    model = Service
+    fields = ["name", "description", "price"]
+    template_name = 'service/products-create.html'
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_200_OK)
-        else:
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    return Response(status=status.HTTP_200_OK)
+    def get_success_url(self):
+        return reverse_lazy('service:products-list')
 
 
-class CreateServiceAPIView(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
-    @staticmethod
-    def get(request: Request):
-        return Response(status=status.HTTP_200_OK, template_name='service/service_create.html')
+class DeleteServiceView(DeleteView):
+    model = Service
+    template_name = 'service/products-delete.html'
+    success_url = '/products/'
 
-    @staticmethod
-    def post(request: Request) -> Response:
-        serializer = ServiceSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(status=status.HTTP_200_OK)
-        else:
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get_success_url(self):
+        return reverse_lazy('service:products-list')
 
 
-class ServiceAPIView(APIView):
-    renderer_classes = [TemplateHTMLRenderer]
+class DetailsServiceView(DetailView):
+    model = Service
+    template_name = 'service/products-detail.html'
+    context_object_name = 'service'
 
-    @staticmethod
-    def get(request: Request, *args, **kwargs) -> Response:
-        print(request.data)
-        print(kwargs)
-        service = Service.objects.get(id=kwargs['pk'])
-        serializer = ServiceSerializer(instance=service)
-        return Response({"service": serializer.data}, status=status.HTTP_200_OK, template_name='service/service_detail.html')
 
-    @staticmethod
-    def put(request: Request, *args, **kwargs) -> Response:
-        service = Service.objects.get(id=kwargs['pk'])
-        serializer = ServiceSerializer(instance=service, data=request.data, partial=True)
+class UpdateServiceView(UpdateView):
+    model = Service
+    fields = ["name", "description", "price"]
+    template_name = 'service/products-edit.html'
 
-        if serializer.is_valid():
-            serializer.save()
-
-        return Response(status=status.HTTP_200_OK)
-
-    @staticmethod
-    def delete(request: Request, *args, **kwargs) -> Response:
-        service = Service.objects.get(id=kwargs['pk'])
-        service.delete()
-        return Response(status=status.HTTP_200_OK)
+    def get_success_url(self):
+        return reverse_lazy('service:products-detail', kwargs={"pk": self.kwargs.get('pk')})
