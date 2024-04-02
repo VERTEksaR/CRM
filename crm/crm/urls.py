5f4778fd48ac86ba140d42685cd05d986182b059
VERTEksaR <14.vert.41@gmail.com>
2024-04-02 16:34:11 +0300

    Удален 'adv/' у adv_company.urls

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('service.urls')),
-    path('adv/', include('adv_company.urls')),
+    path('', include('adv_company.urls')),
 ]
