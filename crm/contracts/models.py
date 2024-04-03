5eb5fc36725c4005bab38d0c648eaf3d6603b8e2
VERTEksaR <14.vert.41@gmail.com>
2024-04-03 14:29:19 +0300

    Добавлена модель для контракта

---
+++ crm/contracts/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+from service.models import Service
+
+
+class Contract(models.Model):
+    class Meta:
+        verbose_name = 'Контракт'
+        verbose_name_plural = 'Контракты'
+
+    name = models.CharField(unique=True, max_length=255, verbose_name='Название контракта')
+    service = models.ForeignKey(Service, on_delete=models.PROTECT, verbose_name='Услуга')
+    file = models.FileField(upload_to='contracts/documents/', verbose_name='Файл')
+    start_date = models.DateField(default=timezone.now, verbose_name='Дата заключения')
+    end_date = models.DateField(default=timezone.now() + timezone.timedelta(days=1), verbose_name='Дата расторжения')
+
+    @staticmethod
+    def summa(pk):
+        product = Service.objects.get(id=pk)
+        return product.price
+
+    def __str__(self):
+        return self.name
