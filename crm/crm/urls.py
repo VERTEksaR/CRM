8264362f559a5288695ffdf5200871ce20d85927
VERTEksaR <14.vert.41@gmail.com>
2024-04-03 14:31:04 +0300

    Добавлено приложение contracts

--- crm/crm/urls.py
+++ crm/crm/urls.py
@@ -21,4 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('service.urls')),
     path('', include('adv_company.urls')),
+    path('', include('users.urls')),
+    path('', include('contracts.urls')),
 ]
