83c5d7878e303daa991bcde4d87ec10e99f61ce1
VERTEksaR <14.vert.41@gmail.com>
2024-04-03 14:28:29 +0300

    Добавлена форма для контрактов

---
+++ crm/contracts/forms.py
@@ -0,0 +1,11 @@
+from django import forms
+
+from contracts.models import Contract
+
+
+class ContractForm(forms.ModelForm):
+    class Meta:
+        model = Contract
+        fields = ["name", "service", "file", "start_date", "end_date"]
+    start_date = forms.DateField()
+    end_date = forms.DateField()
