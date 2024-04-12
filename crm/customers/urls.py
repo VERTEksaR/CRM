5e6806b8f658c67284e15f5e33e7a8565e9870ad
VERTEksaR <14.vert.41@gmail.com>
2024-04-12 13:11:07 +0300

    Добавлено приложение customers

---
+++ crm/customers/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+
+from customers.views import (
+    CustomerListView,
+    CustomerCreateView,
+    CustomerDetailView,
+    CustomerDeleteView,
+    CustomerUpdateView,
+)
+
+
+app_name = 'customers'
+
+urlpatterns = [
+    path('customers/', CustomerListView.as_view(), name='customers-list'),
+    path('customers/<int:pk>/', CustomerDetailView.as_view(), name='customers-detail'),
+    path('customers/<int:pk>/delete/', CustomerDeleteView.as_view()),
+    path('customers/<int:pk>/edit/', CustomerUpdateView.as_view()),
+    path('customers/new/', CustomerCreateView.as_view()),
+]
