17056f5bf825dc75d0bff212d4d74be4b9bc22e0
VERTEksaR <14.vert.41@gmail.com>
2024-04-16 16:15:10 +0300

    .

--- crm/users/admin.py
+++ crm/users/admin.py
@@ -1,19 +1,1 @@
 from django.contrib import admin
-
-from users.models import Management
-
-
-@admin.register(Management)
-class ManagementAdmin(admin.ModelAdmin):
-    list_display = "user", "name", "role"
-    ordering = ["role"]
-    search_fields = "name", "role"
-    fieldsets = [
-        (None, {
-            "fields": ("name",)
-        }),
-        ("Role", {
-            "fields": ("role",),
-            "classes": ("wide",),
-        })
-    ]
