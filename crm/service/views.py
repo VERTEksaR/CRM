bb0b40969388f57d6500408c7bdb21b56aad59da
Valeriy Chekavyi <14.vert.41@gmail.com>
2024-04-21 13:50:58 +0300

    Автоматическое создание ролей пользователей при заходе на главную страницу (если они не были до этого созданы)

--- crm/service/views.py
+++ crm/service/views.py
@@ -1,3 +1,7 @@
+from typing import List
+
+from django.contrib.auth.models import Group, Permission
+from django.contrib.contenttypes.models import ContentType
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -11,12 +15,45 @@ from users.models import Lead
 from users.mixins import GroupRequiredMixin
 
 
+def add_group_and_permissions():
+    names: List[str] = ['Оператор', 'Менеджер', 'Маркетолог', 'Администратор']
+    permissions = [["add_lead", "change_lead", "delete_lead", "view_lead"],
+                   ["add_contract", "change_contract", "delete_contract", "view_contract",
+                    "view_lead", "add_customer", "view_customer"],
+                   ["add_service", "change_service", "delete_service", "view_service",
+                    "add_advcompany", "change_advcompany", "delete_advcompany", "view_advcompany"],
+                   ["add_user", "change_user", "delete_user", "view_user",
+                    "add_permission", "change_permission", "view_permission", "delete_permission"]]
+
+    for name in names:
+        new_group, created = Group.objects.get_or_create(name=name)
+
+        if created:
+            if name == 'Оператор':
+                for perm in range(4):
+                    permission = Permission.objects.get(codename=permissions[0][perm])
+                    new_group.permissions.add(permission)
+            elif name == 'Менеджер':
+                for perm in range(7):
+                    permission = Permission.objects.get(codename=permissions[1][perm])
+                    new_group.permissions.add(permission)
+            elif name == 'Маркетолог':
+                for perm in range(8):
+                    permission = Permission.objects.get(codename=permissions[2][perm])
+                    new_group.permissions.add(permission)
+            elif name == 'Администратор':
+                for perm in range(8):
+                    permission = Permission.objects.get(codename=permissions[3][perm])
+                    new_group.permissions.add(permission)
+
+
 class MainPageView(View):
     """Класс для просмотра основной страницы"""
 
     @staticmethod
     def get(request: HttpRequest) -> HttpResponse:
         """Метод для просмотра основной страницы"""
+        add_group_and_permissions()
         products = Service.objects.all()
         advertisements = AdvCompany.objects.prefetch_related('service').all()
         leads = Lead.objects.select_related('ads').filter(is_active=False)
